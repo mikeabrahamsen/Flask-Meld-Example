190d444b5a1057fb83673d31258c5905d1e7d3b3
Michael Abrahamsen <mail@michaelabrahamsen.com>
2020-12-18 16:58:18 -0700

    remove registrations list

--- meld/components/register.py
+++ meld/components/register.py
@@ -4,7 +4,6 @@ from forms import RegistrationForm
 
 class Register(Component):
     form_class = RegistrationForm
-    registrations = []
 
     def updated(self, field):
         self.validate(field)
